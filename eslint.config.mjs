9c78362325249117fd854bbc6b6973fc125e5f21
Garcia Gudamud Jhon Kenedy <112609873+Jkennedy2004@users.noreply.github.com>
2025-08-08 23:59:08 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,23 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Añade este nuevo objeto para deshabilitar las reglas
+  {
+    rules: {
+      // Desactiva el error de `any`
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // Desactiva las advertencias de variables no usadas
+      "@typescript-eslint/no-unused-vars": "off",
+
+      // Desactiva la advertencia de dependencias de `useEffect`
+      "react-hooks/exhaustive-deps": "off",
+
+      // Desactiva la advertencia de la etiqueta `<img>`
+      "@next/next/no-img-element": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
